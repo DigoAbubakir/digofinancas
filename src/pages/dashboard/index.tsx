23dd96ee991ef3a0c335081626fd9cbfc2f92ec3
DigoAbubakir <63432661+DigoAbubakir@users.noreply.github.com>
2023-03-23 19:42:01 -0300

    Adicionando mais cards

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,16 +1,18 @@
 import { CardDashboard } from "@/src/components/Cards/CardDashboard";
 import SidebarWithHeader from "@/src/components/Sidebar/SidebarWithHeader";
 import { Box, Circle, Heading, HStack, Icon, SimpleGrid, Text } from "@chakra-ui/react";
-import { FiHome } from "react-icons/fi";
+import { FiDatabase,FiChevronsDown, FiChevronsUp, FiArchive } from "react-icons/fi";
 
 export default function Dashboard() {
     return (
         <SidebarWithHeader>
             <Heading mb={5} ml={1}>Dashboard</Heading>
             <SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing="24px" >
-
-                <CardDashboard description="Valor Total" value="R$ 600,00" color="blue.400" icon={FiHome} />
-                <CardDashboard description="Valores a Receber" value="R$ 1500,00" color="red.400" icon={FiHome} />
+                
+                <CardDashboard description="Valor Total" value="R$ 600,00" color="blue.400" icon={FiDatabase} />
+                <CardDashboard description="Valores a Receber" value="R$ 1500,00" color="green.400" icon={FiChevronsUp} />
+                <CardDashboard description="Valores a Pagar" value="R$ 200,00" color="red.400" icon={FiChevronsDown} />
+                <CardDashboard description="Valor Guardado" value="R$ 1000,00" color="purple.400" icon={FiArchive} />
 
             </SimpleGrid>
         </SidebarWithHeader>
