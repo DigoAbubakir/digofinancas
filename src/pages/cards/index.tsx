6838aaf74fa28ebe1bd001f099298fcb4cc2ca2a
DigoAbubakir <63432661+DigoAbubakir@users.noreply.github.com>
2023-03-30 20:32:37 -0300

    adicionando tela de login

--- src/pages/cards/index.tsx
+++ src/pages/cards/index.tsx
@@ -1,12 +1,12 @@
 import SidebarWithHeader from "@/src/components/Sidebar/SidebarWithHeader";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function Dashboard() {
     return (
         <SidebarWithHeader>
-            <Box minH={"300px"} minW="300px" bg='gray.400'>
+            <Flex minH={"300px"} minW="300px" bg='gray.400'>
 
-            </Box>
+            </Flex>
         </SidebarWithHeader>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,79 @@
-export default function Home() {
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  HStack,
+} from '@chakra-ui/react';
+import NextLink from "next/link";
+
+export default function SimpleCard() {
   return (
-    <>
-      <h1>AQUI VAI SER MINHA TELA DE LOGIN</h1>
-    </>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      minW="350px"
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'450px'} minW="400px" py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Bem-vindo</Heading>
+          <Heading fontSize={'4xl'}>Faça seu login</Heading>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Senha</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Lembrar</Checkbox>
+                <Link color={'blue.400'}>Esqueceu sua senha?</Link>
+              </Stack>
+
+
+
+            </Stack>
+
+          </Stack>
+          <HStack justify={"center"} align="center" mt={5}>
+            <NextLink style={{ width: "100%" }} href={"/dashboard"} passHref>
+              <Button
+                w={"100%"}
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Entrar
+              </Button>
+            </NextLink>
+
+          </HStack>
+
+
+        </Box>
+      </Stack>
+    </Flex>
   );
 }
